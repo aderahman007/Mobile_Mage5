54d35ee8dfb347a19f4f21400eaac9bee1e1a622
aderahman007 <aderahman9908@gmail.com>
2019-11-07 01:04:54 +0700

    First Commit

---
+++ app/src/main/java/com/example/mage/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.mage;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigationView =findViewById(R.id.btm_nav);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int id= menuItem.getItemId();
+
+                if (id == R.id.beranda){
+                    BerandaFragment fragment = new BerandaFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+                }
+
+                if (id == R.id.notifikasi){
+                    NotifikasiFragment fragment = new NotifikasiFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+                }
+
+                if (id == R.id.konsultasi){
+                    KonsultasiFragment fragment = new KonsultasiFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+                }
+
+                if (id == R.id.transaksi){
+                    TransaksiFragment fragment = new TransaksiFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+                }
+
+                if (id == R.id.profil){
+                    ProfilFragment fragment = new ProfilFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+                }
+                return true;
+            }
+        });
+
+        navigationView.setSelectedItemId(R.id.beranda);
+    }
+}
