cbcc0680663042940f55b91fc0d78b045600d419
aderahman007 <aderahman9908@gmail.com>
2019-11-11 19:55:25 +0700

    selesai

--- app/src/main/java/com/example/mage/BerandaFragment.java
+++ app/src/main/java/com/example/mage/BerandaFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
  * A simple {@link Fragment} subclass.
  */
 public class BerandaFragment extends Fragment {
+    private RelativeLayout modali;
 
 
     public BerandaFragment() {
@@ -21,10 +23,10 @@ public class BerandaFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_beranda, container, false);
+
     }
 
 }
